f3bdaef551bf8b0adfab70a09950c4452f402798
abshah11997 <abdullahshahh44@gmail.com>
2025-08-05 18:56:31 +0300

    Create 112233445577.py
    this is my first coding journey

---
+++ 112233445577.py
@@ -0,0 +1,30 @@
+age=20
+print(age)
+email='Rongames@gmail.com'
+print(email)
+name='ron'
+print(name)
+A=19
+B=3
+print(A+B)
+print(A-B)
+print(A*B)
+print(A/B)
+name='Alice'
+age=30
+height=5.4
+print('Name:', Name)
+print('Age:', Age)
+print('Height:', Height)
+score = 80
+score=100
+if score > 50:
+    print("You passed")
+else:
+    print( ' you failed')
+ age = 18 
+ age=18
+if age >18:
+     print("you can vote")
+else: 
+     print("you can not vote")
